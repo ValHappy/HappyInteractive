99c0e0f400653af9f0ca03ac0b15e83ebe137d96
Valentina Alegría <val.happy.m@gmail.com>
2021-03-24 00:06:26 -0500

    repository creation

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { withRouter } from 'react-router';
+
+function Home() {
+    return (
+        <div> mi home</div>
+    );
+}
+
+export default withRouter(Home);
